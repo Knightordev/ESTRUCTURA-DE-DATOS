af484afd75886400ec64b2a4d4ba1042fcf1d386
Héctor Iván Chumba Poot <hichumba@gmail.com>
2025-09-22 08:29:40 -0600

    bettercode-Arreglos

--- Unidad01/EJER - ARREGLOS/arreglos.py
+++ Unidad01/EJER - ARREGLOS/arreglos.py
@@ -14,9 +14,8 @@ def show_sales_table():
         print("{:<12} {:<10} {:<10} {:<10}".format(*fila))
 
 def add_sales():
+    month_index, sales_index = ask()
     print("////añadiendo venta mensual////")
-    month_index = int(input("Ingrese el mes 1-12 siendo 1-enero y 12-diciembre: "))
-    sales_index = int(input(f"Ingrese a donde fue la venta \n 1 .- {monthly_sales[0][1]}, \n 2 .- {monthly_sales[0][2]}, \n 3 .- {monthly_sales[0][3]} \n indice: "))
     if(does_this_sale_exist(month_index, sales_index)):
         sales_to_set = int(input("Ingrese las ventas totales: "))
         monthly_sales[month_index][sales_index] = sales_to_set
@@ -24,27 +23,28 @@ def add_sales():
 
 def look_sale():
     print("////BUscar venta mensual////")
-    month_index = int(input("Ingrese el mes 1-12 siendo 1-enero y 12-diciembre: "))
-    sales_index = int(input(f"Ingrese a donde fue la venta \n 1 .- {monthly_sales[0][1]}, \n 2 .- {monthly_sales[0][2]}, \n 3 .- {monthly_sales[0][3]} \n indice: "))
+    month_index, sales_index = ask()
     if(does_this_sale_exist(month_index, sales_index)):
         return f"venta del mes {monthly_sales[month_index][0]} de {monthly_sales[0][sales_index]} con ventas de {monthly_sales[month_index][sales_index]}" 
 
 def delete_sales():
     print("////Eliminar venta mensual////")
-    month_index = int(input("Ingrese el mes 1-12 siendo 1-enero y 12-diciembre: "))
-    sales_index = int(input(f"Ingrese a donde fue la venta \n 1 .- {monthly_sales[0][1]}, \n 2 .- {monthly_sales[0][2]}, \n 3 .- {monthly_sales[0][3]} \n indice: "))
+    month_index, sales_index = ask()
     if(does_this_sale_exist(month_index, sales_index)):
         monthly_sales[month_index][sales_index] = "sin venta"
         print("Eliminado!")
 
 def does_this_sale_exist(month_index, sales_index):
-        if(month_index < 1 or month_index > 12):
-            return False
-        else:
-            if(sales_index < 1 or sales_index > 3):
+            if month_index < 1 or month_index > 12:
                 return False
-            else:
-                return True
+            if sales_index < 1 or sales_index > 3:
+                return False
+            return True
+def ask():
+    month_index = int(input("Ingrese el mes 1-12 siendo 1-enero y 12-diciembre: "))
+    sales_index = int(input(f"Ingrese a donde fue la venta \n 1 .- {monthly_sales[0][1]}, \n 2 .- {monthly_sales[0][2]}, \n 3 .- {monthly_sales[0][3]} \n indice: "))
+    return month_index, sales_index
+
 add_sales()
 show_sales_table()
 print(look_sale())
