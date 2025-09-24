00ab85252c3d6a0146d3188c7a6dc8925fa252b2
Héctor Iván Chumba Poot <hichumba@gmail.com>
2025-09-23 19:31:27 -0600

    Update arreglos.py

--- Unidad01/EJER - ARREGLOS/arreglos.py
+++ Unidad01/EJER - ARREGLOS/arreglos.py
@@ -10,26 +10,26 @@ for i in range(1, month+1):
     monthly_sales[i][0] = month_names[i]
 def show_sales_table():
     print("////Mostrar tabla////")
-    for fila in monthly_sales:
-        print("{:<12} {:<10} {:<10} {:<10}".format(*fila))
+    for row in monthly_sales:
+        print("{:<12} {:<10} {:<10} {:<10}".format(*row))
 
 def add_sales():
-    month_index, sales_index = ask()
+    month_index, sales_index = ask_month_and_department()
     print("////añadiendo venta mensual////")
     if(does_this_sale_exist(month_index, sales_index)):
         sales_to_set = int(input("Ingrese las ventas totales: "))
         monthly_sales[month_index][sales_index] = sales_to_set
         print("Añadido!")
 
-def look_sale():
+def search_sale():
     print("////BUscar venta mensual////")
-    month_index, sales_index = ask()
+    month_index, sales_index = ask_month_and_department()
     if(does_this_sale_exist(month_index, sales_index)):
         return f"venta del mes {monthly_sales[month_index][0]} de {monthly_sales[0][sales_index]} con ventas de {monthly_sales[month_index][sales_index]}" 
 
 def delete_sales():
     print("////Eliminar venta mensual////")
-    month_index, sales_index = ask()
+    month_index, sales_index =ask_month_and_department()
     if(does_this_sale_exist(month_index, sales_index)):
         monthly_sales[month_index][sales_index] = "sin venta"
         print("Eliminado!")
@@ -40,14 +40,14 @@ def does_this_sale_exist(month_index, sales_index):
             if sales_index < 1 or sales_index > 3:
                 return False
             return True
-def ask():
+def ask_month_and_department():
     month_index = int(input("Ingrese el mes 1-12 siendo 1-enero y 12-diciembre: "))
     sales_index = int(input(f"Ingrese a donde fue la venta \n 1 .- {monthly_sales[0][1]}, \n 2 .- {monthly_sales[0][2]}, \n 3 .- {monthly_sales[0][3]} \n indice: "))
     return month_index, sales_index
 
 add_sales()
 show_sales_table()
-print(look_sale())
+print(search_sale())
 show_sales_table()
 delete_sales()
 show_sales_table()
